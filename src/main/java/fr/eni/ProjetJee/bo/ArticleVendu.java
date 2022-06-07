2f451c63180e432b2aa2274f0aa3c79bb53d92ec
Nene Raby DIALLO <neneraby.diallo2021@campus-eni.fr>
2022-06-07 16:53:02 +0200

    articles

--- src/main/java/fr/eni/ProjetJee/bo/ArticleVendu.java
+++ src/main/java/fr/eni/ProjetJee/bo/ArticleVendu.java
@@ -1,5 +1,19 @@
 package fr.eni.ProjetJee.bo;
 
-public class ArticleVendu {
+import java.util.Date;
 
+public class ArticleVendu {
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private Date dateDebutEncheres;
+	private Date dateFinEncheres;
+	private int miseAPrix;
+	private int prixDeVente;
+	private String etatVente;
+	private String photo;
+	private int noUtilisateur;
+	private Retrait lieuRetrait;
+	private Categorie categorie;
+	
 }

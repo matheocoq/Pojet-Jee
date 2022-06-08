63f0411a53de1ef6e1c5b5f837c48b5ceb8b04ec
Matheo COQUEL <matheo.coquel2021@campus-eni.fr>
2022-06-08 09:24:19 +0200

    modif ArticleVenduDAO

--- src/main/java/fr/eni/ProjetJee/dal/ArticleVenduDAO.java
+++ src/main/java/fr/eni/ProjetJee/dal/ArticleVenduDAO.java
@@ -1,8 +1,9 @@
-package fr.eni.ProjetJee.bll;
+package fr.eni.ProjetJee.dal;
 
 import java.util.ArrayList;
 
 import fr.eni.ProjetJee.bo.ArticleVendu;
+import fr.eni.ProjetJee.bo.Categorie;
 import fr.eni.ProjetJee.bo.Utilisateur;
 
 public interface ArticleVenduDAO {
@@ -10,6 +11,7 @@ public interface ArticleVenduDAO {
 	ArticleVendu select(int id) throws DALException;
 	ArrayList<ArticleVendu> select() throws DALException;
 	ArrayList<ArticleVendu> selectByUtilisateur(Utilisateur utilisateur) throws DALException;
+	ArrayList<ArticleVendu> selectByCategorie(Categorie categorie) throws DALException;
 	void delete(int id) throws DALException; 
 	void update(ArticleVendu article) throws DALException; 
 }
